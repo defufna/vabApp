e71fa77d014d1eca110da9d19d729fc4fcae6956
Ivan Savu <ivan.savu@gmail.com>
2023-02-07 21:50:46 +0100

    Writing the client

--- vabApp/Client/Program.cs
+++ vabApp/Client/Program.cs
@@ -6,9 +6,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("vabApp.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+builder.Services.AddHttpClient("vabApp.WebAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("vabApp.ServerAPI"));
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("vabApp.WebAPI"));
 
 await builder.Build().RunAsync();
